05ae9ae4470cf767aef6fe57423aa7d2755c1956
John Andersen <johnandersenpdx@gmail.com>
2018-12-18 09:15:21 -0800

    example: run.sh success if Tss2_Sys_GetCapability

--- example/run.sh
+++ example/run.sh
@@ -2,4 +2,5 @@
 
 sh lib/swtpm2.sh &
 sed -i 's/-vnc.*/-nographic \\/g' lib/qemu-tpm2.sh
-exec timeout 30s unbuffer sh lib/qemu-tpm2.sh
+timeout 30s unbuffer sh lib/qemu-tpm2.sh | grep -q 'Tss2_Sys_GetCapability returned TSS2_RC_SUCCESS'
+exit $?
